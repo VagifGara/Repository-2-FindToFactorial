8cce6a2fa5565283b57c418a0fff3aa266e7d378
Vagif0728 <tu7j4ryqj@code.edu.az>
2023-12-18 04:31:40 +0400

    Task : The number is find to factorial

---
+++ FactorialTask/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FactorialTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ededi daxil edin: ");
+            int.TryParse(Console.ReadLine(), out int number);
+
+            Console.WriteLine(Factorial(number));
+
+        }
+
+       
+
+     public  static int Factorial(int num)
+        {
+            if (num == 0 || num ==1)
+            {
+                return 1;
+            }
+            return num * Factorial(num - 1);
+        }
+    }
+}
